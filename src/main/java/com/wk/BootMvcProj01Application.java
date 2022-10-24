549f262fb9c33b76e2aff7fd5d5ad31fa2b9a4a0
Himanshu Choudhary <toreachmanas@gmail.com>
2022-10-24 09:19:43 +0530

    first commit

---
+++ src/main/java/com/wk/BootMvcProj01Application.java
@@ -0,0 +1,13 @@
+package com.wk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj01Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/wk/controller/ShowHomeController.java
@@ -0,0 +1,13 @@
+package com.wk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class ShowHomeController {
+
+	@RequestMapping("/")
+	public String showHome() {
+		
+		return "home";
+	}
+}
